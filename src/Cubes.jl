494771a4e7ac8c6949536ec090c97959ce3ee606
Jose A Luna <tsukisan@gmail.com>
2025-09-30 10:53:45 +0300

    Basic state functionality

--- src/Cubes.jl
+++ src/Cubes.jl
@@ -1,17 +1,34 @@
 module Cubes
+export Cube, to_full, to_zero, isSolutionQ, fromList, fromPair, State, isSolutionQ
+## --------------------------------------------------------
+## -------------------Cube struct -------------------------
+## --------------------------------------------------------
+const Capacity = Int64     # Alias for the type
 
-# Write your package code here.
-
-const Capacity = Int64
-
-struct Cubes
-    capacity::Capacity
-    amount::Capacity
+struct Cube                # Basic structure for the cubes
+    capacity::Capacity     # Total capacity
+    amount::Capacity       # How much is carrying
 end
-const ListOfCubes = Array{Cube,1s}
+
+# To simplify the creation of states
+const ListOfCubes = Vector{Cube}
+
 
 to_full(cube::Cube) = Cube(cube.capacity, cube.capacity)
 to_zero(cube::Cube) = Cube(cube.capacity, 0)
+
+fromList(a) = sort(map(e -> Cube(e,0),a), by=cube -> cube.capacity)
+fromPair(pairs) = sort(map(p -> Cube(p...),pairs), by=cube -> cube.capacity)
+
+## --------------------------------------------------------
+## -------------------State struct ------------------------
+## --------------------------------------------------------
+struct State
+    cubes::ListOfCubes   # Cubes with the capacities and amounts
+    goal::Capacity       # Which is our goal
+end
+# is a solution
+isSolutionQ(state::State, goal::Capacity) = any(cube -> cube.amount == goal, state.cubes)
 
 
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,26 @@
 using Cubes
 using Test
 
+
+
+## Some examples
+state_simple_sol = State(fromPair([[2, 1],[3,2],[7,5]]),5)
+
+
+@testset "Bases" begin
+
+    @test 1 == 1
+
+end
+
 @testset "Cubes.jl" begin
     # Write your tests here.
-    @test 1 == 1
+    @test fromList([1, 2]) == [Cube(1,0),Cube(2,0)]                         # ordered
+    @test fromList([9,2,3]) == [Cube(2,0),Cube(3,0),Cube(9,0)]              # not ordered
+    @test fromPair([[2, 1],[3,2]]) == [Cube(2,1),Cube(3,2)]                 # ordered
+    @test fromPair([[2, 1],[3,2],[7,5]]) == [Cube(2,1),Cube(3,2),Cube(7,5)] # ordered
+    @test fromPair([[7,5],[2, 1],[3,2]]) == [Cube(2,1),Cube(3,2),Cube(7,5)] # not ordered
+    @test state_simple_sol.goal == 5
+    @test isSolutionQ(state_simple_sol,5)
+    @test !isSolutionQ(state_simple_sol,3)
 end
