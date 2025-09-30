0da858ab65358394702d5f865c73966a2fcc9d55
Jose A Luna <tsukisan@gmail.com>
2025-09-30 12:01:20 +0300

    Tracking the visited states

--- src/Cubes.jl
+++ src/Cubes.jl
@@ -1,5 +1,6 @@
 module Cubes
-export Cube, to_full, to_zero, isSolutionQ, fromList, fromPair, State, isSolutionQ
+export Cube, to_full, to_zero, isSolutionQ, fromList, fromPair, State, 
+       isSolutionQ, cubes_hash, Visited, add_to_visited, addStateToVisited, hasBeenVisited
 ## --------------------------------------------------------
 ## -------------------Cube struct -------------------------
 ## --------------------------------------------------------
@@ -13,6 +14,13 @@ end
 # To simplify the creation of states
 const ListOfCubes = Vector{Cube}
 
+cubes_hash(cubes::ListOfCubes) = reduce(vcat, map(cube->[cube.capacity, cube.amount],cubes))
+
+const Visited = Set{Vector{Capacity}}
+function add_to_visited(set::Visited, cubes::ListOfCubes)
+    hash = cubes_hash(cubes)
+    push!(set, hash)
+end
 
 to_full(cube::Cube) = Cube(cube.capacity, cube.capacity)
 to_zero(cube::Cube) = Cube(cube.capacity, 0)
@@ -29,6 +37,19 @@ struct State
 end
 # is a solution
 isSolutionQ(state::State, goal::Capacity) = any(cube -> cube.amount == goal, state.cubes)
+
+addStateToVisited(set::Visited, state::State) = add_to_visited(set, state.cubes)
+
+hasBeenVisited(set::Visited, state::State)    = in(state.cubes |> cubes_hash, set)
+
+
+# Basic functionality for checking if a state has been in the space of states
+# Consider for easy a set of flatted arrays sorted
+
+
+
+
+
 
 
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,7 @@
 using Cubes
 using Test
 
-
-
-## Some examples
-state_simple_sol = State(fromPair([[2, 1],[3,2],[7,5]]),5)
-
-
+    
 @testset "Bases" begin
 
     @test 1 == 1
@@ -14,6 +9,9 @@ state_simple_sol = State(fromPair([[2, 1],[3,2],[7,5]]),5)
 end
 
 @testset "Cubes.jl" begin
+    # Some data
+    state_simple_sol = State(fromPair([[2, 1],[3,2],[7,5]]),5)
+
     # Write your tests here.
     @test fromList([1, 2]) == [Cube(1,0),Cube(2,0)]                         # ordered
     @test fromList([9,2,3]) == [Cube(2,0),Cube(3,0),Cube(9,0)]              # not ordered
@@ -23,4 +21,31 @@ end
     @test state_simple_sol.goal == 5
     @test isSolutionQ(state_simple_sol,5)
     @test !isSolutionQ(state_simple_sol,3)
+
+    
+
+end
+
+@testset "Visited States" begin
+    ## Some examples
+    state_simple_sol = State(fromPair([[2, 1],[3,2],[7,5]]),5)
+    visited::Visited = Set()
+    # At the very begining the state has not been visited
+    @test !hasBeenVisited(visited, state_simple_sol)
+    @test (state_simple_sol.cubes |> cubes_hash) == [2, 1, 3, 2, 7, 5]
+    # See how the set track the states which has been visited
+    @test (state_simple_sol.cubes |> cubes_hash) == reduce(vcat, [[2, 1],[3,2],[7,5]])
+    add_to_visited(visited, state_simple_sol.cubes)
+    @test length(visited) == 1
+    addStateToVisited(visited, state_simple_sol)
+    @test length(visited) == 1
+    @test hasBeenVisited(visited, state_simple_sol)
+    add_to_visited(visited,fromPair([[2, 1],[3,2],[7,5]]))
+    @test length(visited) == 1
+    add_to_visited(visited,fromPair([[2, 1],[3,2],[7,7]]))
+    @test length(visited) == 2
+
+
+
+
 end
